f3c0c969b105210bf6da8de9bd06866a4fc9ea1e
EC2 Default User <ec2-user@ip-172-31-35-101.us-east-2.compute.internal>
2020-05-02 14:21:25 +0000

    cf_lab3

--- cf_lab3/deploy.sh
+++ cf_lab3/deploy.sh
@@ -55,8 +55,8 @@ if [[ $d -eq 1 ]]; then
   aws cloudformation deploy \
     --no-fail-on-empty-changeset \
     --template-file $CF_FILE \
-    --parameter-overrides Project=cf_lab2 \
-    --stack-name "my-stack-shell" \
+    --parameter-overrides Project=cf_lab3 \
+    --stack-name "my-stack-shell2" \
     --capabilities CAPABILITY_NAMED_IAM
 fi
 
